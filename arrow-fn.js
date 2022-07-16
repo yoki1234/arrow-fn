6b8fb210b1fc9ebbd5754f6a8c389640c1808936
akshay_ashok <akshayashokdj@gmail.com>
2022-07-16 23:53:36 +0530

    arrow

---
+++ arrow-fn.js
@@ -0,0 +1,16 @@
+const sum = (a,b) => a+b;
+console.log(sum(2,2));
+
+const num = number => number > 10;
+console.log(num(22));
+
+const a = (a,b,c) => a+b-c;
+console.log(a(1,5,2));
+
+const b =()=>{
+    let a=3;
+    let b=2;
+    return a+b;
+
+};
+console.log(b());
